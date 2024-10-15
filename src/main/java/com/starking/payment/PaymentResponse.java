6e552663de070715add619eed41138aa2e508c5d
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-15 20:44:29 -0300

    PaymentResponse

--- src/main/java/com/starking/payment/PaymentResponse.java
+++ src/main/java/com/starking/payment/PaymentResponse.java
@@ -1,4 +1,17 @@
 package com.starking.payment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentResponse {
+
+    private Boolean success;
+    private String transactionId;
+    private String message;
 }
