1c773677718729df03b763752197883353574527
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-15 20:47:37 -0300

    processPayment implementado

--- src/main/java/com/starking/payment/StripePaymentService.java
+++ src/main/java/com/starking/payment/StripePaymentService.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class StripePaymentService implements PaymentService {
 
-
     @Override
     public PaymentResponse processPayment(PaymentRequest request) {
-        return null;
+        PaymentResponse response = new PaymentResponse();
+        response.setSuccess(true);
+        response.setTransactionId("txn_123456789");
+        response.setMessage("Pagamento realizado com sucesso.");
+        return response;
     }
 }
