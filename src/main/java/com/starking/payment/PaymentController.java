b425303f06bb758dc0ddd1292c35d07c24ce80ac
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-15 21:04:08 -0300

    processPayment implementado

---
+++ src/main/java/com/starking/payment/PaymentController.java
@@ -0,0 +1,26 @@
+package com.starking.payment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentResponse> processPayment(@RequestBody PaymentRequest request) {
+        PaymentResponse response = paymentService.processPayment(request);
+        return ResponseEntity.ok(response);
+    }
+}
